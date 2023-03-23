a2ce626debc3b9a5926f17203efe10f50495117a
Lucinda <122218242+LucindaBury@users.noreply.github.com>
2023-03-23 14:25:03 +0000

    Most recent edit with updated Vues

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex flex-col space-y-4">
+        <Pokemon :pokemon="pokemon" v-for="(pokemon, index) in pokemons" :key="index" />
+    </div>
+</template>
+
+<script setup>
+// import pokemons from "@/assets/sampledataset.json"
+import Pokemon from "./Pokemon.vue"
+
+import {ref, onMounted} from "vue"
+import client from "@/assets/apiclient.js"
+const pokemons = ref([])
+
+// mount and then make api call
+
+onMounted(()=>{
+    client.get("/pokemon").then(res => {
+        console.log("response recieved", res.data),
+        pokemons.value = res.data
+}).catch(errors =>{
+        console("errors", errors)
+    })
+})
+</script>
